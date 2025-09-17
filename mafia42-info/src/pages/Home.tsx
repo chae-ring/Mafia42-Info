e79bd6a6917153ab8cae954101284299195f2168
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-09-17 18:58:25 +0900

    fix: 버튼 이름 수정

--- mafia42-info/src/pages/Home.tsx
+++ mafia42-info/src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import "./Home.css";
 
 const Home = () => {
-  const channels = ["Channel 1", "Channel 2", "Channel 3", "Channel 4"];
+  const channels = [
+    "계산기",
+    "카드강화 시뮬레이션",
+    "컬렉션 & 레벨보상",
+    "업데이트 및 게임 내 정보",
+  ];
 
   return (
     <div className="home-container">
